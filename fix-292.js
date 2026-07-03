840202ca2bb2123be27de4afd879bd63a04b9f50
Rahma Cahyani <mfaisalyahya123@gmail.com>
2026-07-04 00:02:30 +0700

    Add: gaji minggu 7 (29Jun-4Jul) Rp 7.74jt - Heri, Supar, Sueb, Muji, Jo, Rudi, Riski, Adit, Ahmad

--- fix-292.js
+++
@@ -1,9 +0,0 @@
-const d = JSON.parse(require('fs').readFileSync('kos-um/data.json','utf8'));
-const tx = d.transactions.find(t => t.id === 292);
-if (tx) {
-    tx.quantity = 1;
-    tx.unit = 'dus';
-    tx.funding_source = 'Kas UM';
-    console.log('Updated ID 292:', tx.description, '|', tx.quantity, tx.unit, '|', tx.funding_source);
-}
-require('fs').writeFileSync('kos-um/data.json', JSON.stringify(d, null, 2));
